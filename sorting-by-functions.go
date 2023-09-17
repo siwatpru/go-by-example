e50e799a409971ed21a9fbe554dfe0fd4b6f5c28
Siwat Pruksapanya <siwat.pru@gmail.com>
2023-09-17 19:43:42 +0700

    Default sort and sort by functions

---
+++ sorting-by-functions.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"cmp"
+	"fmt"
+	"slices"
+)
+
+func main() {
+	fruits := []string{"peach", "banana", "kiwi"}
+
+	lenCmp := func(a, b string) int {
+		return cmp.Compare(len(a), len(b))
+	}
+
+	slices.SortFunc(fruits, lenCmp)
+	fmt.Println(fruits)
+
+	type Person struct {
+		name string
+		age  int
+	}
+
+	people := []Person{
+		{"Bob", 31},
+		{"John", 42},
+		{"Michael", 17},
+	}
+
+	slices.SortFunc(people, func(a, b Person) int {
+		return cmp.Compare(a.age, b.age)
+	})
+	fmt.Println(people)
+}
